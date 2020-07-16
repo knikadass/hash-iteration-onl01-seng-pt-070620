bcb195664d2214ca6881a9029953e29aa3dbc417
Knika Dass <knikadass@gmail.com>
2020-07-16 00:17:41 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -25,7 +25,7 @@ describe "#age_appropriate_birthday" do
     
     def age_appropriate_birthday(birthday_kids)
       birthday_kids.each do |kids_name, age|
-      
+       if kids_name = 
 
     expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
