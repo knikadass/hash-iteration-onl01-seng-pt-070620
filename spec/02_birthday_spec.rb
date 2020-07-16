c42432055c98fc1240e98cbf14b48565ca681ac1
Knika Dass <knikadass@gmail.com>
2020-07-16 00:22:35 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -26,7 +26,7 @@ describe "#age_appropriate_birthday" do
     
     def age_appropriate_birthday(birthday_kids)
       birthday_kids.each do |kids_name, age|
-        if kids_name
+        if age > 12
          puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
